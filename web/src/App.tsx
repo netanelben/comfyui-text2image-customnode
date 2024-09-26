c51c99436c9b5de7986375f46ebb40d08efc1da4
Nate Ben <netanelben@users.noreply.github.com>
2024-09-26 14:38:44 +0300

    -

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import workflow from "./workflows/workflow_api.json";
 import "./App.css";
+import { MessageData } from "./types";
 
 const clientUniqueId = uuidv4();
 
 function App() {
   const [text, settext] = useState("");
   const [imageSrc, setimageSrc] = useState<string | null>(null);
+  const [progress, setprogress] = useState({ value: 0, max: 0 });
 
   useEffect(() => {
     console.log("🧩 Loading workflow", workflow);
@@ -23,22 +25,30 @@ function App() {
     };
 
     wsClient.addEventListener("message", (event) => {
-      const data = JSON.parse(event.data);
+      const data = JSON.parse(event.data) as MessageData;
       console.log("📡 Message from server", data);
+
+      if (data.type === "progress") {
+        trackProgress(data.data.value, data.data.max);
+      }
 
       if (data.type === "executed") {
         if ("images" in data.data.output) {
           const image = data.data.output.images[0];
-          const filename = image.filename;
-          const subfolder = image.subfolder;
+          const { filename, type, subfolder } = image;
           const rando = Math.floor(Math.random() * 1000);
-          const imageSrc = `/view?filename=${filename}&type=output&subfolder=${subfolder}&rand=${rando}`;
+          const imageSrc = `/view?filename=${filename}&type=${type}&subfolder=${subfolder}&rand=${rando}`;
 
           setimageSrc(imageSrc);
         }
       }
     });
   }, []);
+
+  const trackProgress = (value: number, max: number) => {
+    console.log(`🚦 Generate progress ${value}/${max}`);
+    setprogress({ value, max });
+  };
 
   const handleTextChange = (e: any) => {
     const text = e.target.value;
@@ -47,14 +57,34 @@ function App() {
 
   const handleGenerate = async (e: any) => {
     if (!text) return;
+    if (typeof workflow === "undefined") return;
 
-    const results = await queuePrompt(text, clientUniqueId);
+    // Find the key of prompt node
+    const inputNodeNumber = Object.entries(workflow).find(
+      ([key, value]) => value.class_type === "CLIPTextEncode"
+    )[0] as keyof typeof workflow;
+
+    // Find the key of KSampler node
+    const samplerNodeNumber = Object.entries(workflow).find(
+      ([key, value]) => value.class_type === "KSampler"
+    )[0] as keyof typeof workflow;
+
+    workflow[inputNodeNumber].inputs.text = text.replaceAll(
+      /\r\n|\n|\r/gm,
+      " "
+    );
+
+    workflow[samplerNodeNumber].inputs.seed = Math.floor(
+      Math.random() * 9999999999
+    );
+
+    const results = await queuePrompt(workflow);
     console.log({ results });
     // setimageSrc();
   };
 
-  async function queuePrompt(prompt: string, clientId: string) {
-    const data = { prompt, client_id: clientId };
+  async function queuePrompt(workflow = {}) {
+    const data = { prompt: workflow, client_id: clientUniqueId };
 
     const response = await fetch("/prompt", {
       method: "POST",
@@ -108,7 +138,6 @@ function App() {
         />
       )}
       <textarea
-        type="text"
         placeholder="Prompt"
         onChange={handleTextChange}
         style={{
@@ -124,6 +153,7 @@ function App() {
       >
         <button onClick={handleGenerate} style={{ flex: 1 }}>
           Generate
+          {progress.value > 0 && ` (${progress.value}/${progress.max})`}
         </button>
         <button onClick={interruptPrompt}>❌</button>
       </div>
